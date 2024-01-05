32a42ec45216f48408a255eb06c1d62e66004af7
nursbisen <nursatnba@gmail.com>
2024-01-05 17:17:11 +0600

    05.01.24 by Rustam

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import { Table, HeaderCell, Cell, Column } from "rsuite-table";
+import 'rsuite-table/dist/css/rsuite-table.css';
 
 import Layout from "./views/Layout";
 import Banner from "./views/Banner";
@@ -13,17 +16,34 @@ import Offices from "./views/Offices";
 const App = () => {
 
     return (
-        <Layout>
-            <Banner />
-            <AboutCompany />
-            <Goals />
-            <Technologies />
-            <Request />
-            <Advantages />
-            <Education />
-            <Request />
-            <Offices />
-        </Layout>
+        <div>
+            <Helmet htmlAttributes>
+                <html lang="ru" />
+                <title>RE/MAX</title>
+                <meta name="description" content="Бизнес-модель RE/MAX, признанная лучшей в мире, основана на уникальном ноу-хау и слаженной системе, что позволяет компании сохранять лидерские позиции на протяжении уже 50 лет в более чем 119 странах мира." />
+            </Helmet>
+            <Layout>
+                <Banner />
+                <Table data={[{ id: "1", xa: "Nurs" }, { id: "3", xa: "Bisen" }]}>
+                <Column>
+                  <HeaderCell>WS</HeaderCell>
+                  <Cell dataKey="id" />
+                </Column>
+                <Column>
+                  <HeaderCell>Opapapa</HeaderCell>
+                  <Cell dataKey="xa" />
+                </Column>
+              </Table>
+                <AboutCompany />
+                <Goals />
+                <Technologies />
+                <Request />
+                <Advantages />
+                <Education />
+                <Request />
+                <Offices />
+            </Layout>
+        </div>
     );
 };
 
